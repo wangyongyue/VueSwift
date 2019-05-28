a2ef9d1865f11a921c7dabca4ae35e12b528909f
wangyongyue <wangyouyue@kingbirdplus.com>
2019-05-28 13:54:58 +0800

    Initial Commit

---
+++ VueSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VueSwift
+//
+//  Created by apple on 2019/5/28.
+//  Copyright © 2019 wyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ VueSwiftTests/VueSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  VueSwiftTests.swift
+//  VueSwiftTests
+//
+//  Created by apple on 2019/5/28.
+//  Copyright © 2019 wyy. All rights reserved.
+//
+
+import XCTest
+@testable import VueSwift
+
+class VueSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
